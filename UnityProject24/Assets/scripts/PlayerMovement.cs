980ca6516cafe0466cd561c0970fe0fe77a2c2cd
JaydenStenschke <jayden.stenschke@hgs.hiddengeniusproject.org>
2024-07-19 14:28:52 -0700

    Pushing my progress
    Trying to push my progress

---
+++ UnityProject24/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody rb;
+    Vector3 movement;
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
